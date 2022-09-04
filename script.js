7385767b9463cf2c01555187470e4b85c27a00d6
bmilcs <bmilcs@yahoo.com>
2022-09-04 12:18:54 -0400

    Add success/error functions & basic submit eventlistener

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-const form = document.getElementById("signup-form");
+const signupForm = document.getElementById("signup-form");
 const firstName = document.getElementById("first_name");
 const lastName = document.getElementById("last_name");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const password = document.getElementById("password");
 const passwordConfirm = document.getElementById("password_confirm");
+const input = document.querySelectorAll("input");
 
 // turn off default error messages
-form.noValidate = true;
+signupForm.noValidate = true;
 
 // todo
 // on submit, validate all fields
 // on focus change, check validity
 // after focus change, swap to aggressive error checking
 
-// functions
+const displayError = (field, message) => {
+  const errorSpan = field.parentElement.getElementsByTagName("span")[0];
+  errorSpan.textContent = message;
+  field.classList.remove("success");
+  field.classList.add("error");
+};
 
-// displayError(field, message)
-//  display message in the field's corresponding span element
-//  add error class, remove success class
+const displaySuccess = (field) => {
+  const errorSpan = field.parentElement.getElementsByTagName("span")[0];
+  errorSpan.textContent = "";
+  field.classList.remove("error");
+  field.classList.add("success");
+};
 
-// displaySuccess(field)
-//  remove error message from corresponding span
-//  remove error class, add success class
+// validate form on submission
+const validateForm = (e) => {
+  const form = e.target;
 
-// validateFields()
-//  check each field for validity
-//  if invalid, displayError
-//  if valid, displaySuccess
+  if (!form.checkValidity()) {
+    // form is invalid
+    //  check each field for validity
+    //  if invalid, displayError
+  } else {
+    input.forEach((i) => {
+      i.classList.add("success");
+      i.classList.remove("error");
+    });
+  }
+  e.preventDefault();
+  e.stopImmediatePropagation();
+};
+
+// validate form on submit
+signupForm.addEventListener("submit", validateForm);
